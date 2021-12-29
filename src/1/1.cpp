cb1c440e51b32e576bd1a4e64d50163865a64547
念歌 <niange@qunhemail.com>
2021-12-29 13:17:09 +0800

    feat: 1

---
+++ src/1/1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // endl:end of line
+    cout << "Hello World! I am " << 18 << " Today!" << endl;
+    for (;;)
+        ;
+    return 0;
+}
---
+++ src/1/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int inputNumber;
+    cout << "Input a inputNumber:" << endl;
+    cin >> inputNumber;
+    cout << "The inputNumber is " << inputNumber << endl;
+
+    return 0;
+}
